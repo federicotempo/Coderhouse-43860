ddee5a18014259f0d2605e4cd9f65b2b2df9159d
Federico Tempo <federicotempo98@gmail.com>
2023-06-22 20:32:13 -0300

    Agregando urls

---
+++ proyect/config/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+
+def saludo(request):
+	return HttpResponse("Hola Django - Coder")
+
+def segunda_vista(request):
+	return HttpResponse("<h1>Segunda vista<h1>")
+
+def nombre(request, nombre: str, apellido: str):
+    nombre = nombre.capitalize()
+    apellido = apellido.capitalize()
+    return HttpResponse(f"{apellido}, {nombre}")
